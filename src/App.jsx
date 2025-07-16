1827476c8862a816bf493fe46718106a1d33159c
محمد فتحي صلاح <130288085+mohamedfathi0121@users.noreply.github.com>
2025-07-17 01:45:54 +0300

    start chat ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeProvider";
+
 
 function App() {
   return (
